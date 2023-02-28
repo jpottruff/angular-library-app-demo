d7e5c7840773ea00492465cb519cf77a07d2b597
Jeff Pottruff <jeffpottruff@gmail.com>
2023-02-27 22:56:34 -0500

    small modification to general components

--- projects/general-components/src/lib/general-components.component.ts
+++ projects/general-components/src/lib/general-components.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'lib-general-components',
   template: `
     <p>
-      A very awesome general component - that will have live reloading if i use the --watch flag
+      A very awesome general component - that will have live reloading if i use the 
+      <code style="background-color: #D3D3D3">--watch</code> 
+      flag
     </p>
   `,
   styles: [
