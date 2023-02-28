cbde67a9431560997b918da33cf9d50f021e8454
Jeff Pottruff <jeffpottruff@gmail.com>
2023-02-27 21:34:02 -0500

    adds explanatory comments in public-api and tsconfig

--- projects/general-components/src/public-api.ts
+++ projects/general-components/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of general-components
  */
 
+// This defines what is available to the consumers of this library
 export * from './lib/general-components.service';
 export * from './lib/general-components.component';
 export * from './lib/general-components.module';
