23dfd9289c4c89adc06a0fd9103046dc7a87d10d
Jeff Pottruff <jeffpottruff@gmail.com>
2023-02-28 22:22:16 -0500

    modify library and bump version

--- workspace-a/projects/general-components/src/lib/general-components.component.ts
+++ workspace-a/projects/general-components/src/lib/general-components.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-general-components',
   template: `
+    <h2>Version 2:</h2>
     <p>
       A very awesome general component - that will have live reloading if i use the 
       <code style="background-color: goldenrod">--watch</code> 
       flag
     </p>
+    
   `,
   styles: [
   ]
